d5701d2366af064db985b57231110974b7f467bc
Steven Beeckman <steven.beeckman+github@gmail.com>
2012-11-19 07:00:44 +0100

    Added localStorage.getItem() calls again to make clear it doesn't work.

--- js/register.js
+++ js/register.js
@@ -45,23 +45,40 @@ function registerUser(){
 
 function registerCarBrand(brand){
     $("#activityindicator").slideToggle("slow", "swing");
+    var car = new Object();
+    car.brand = brand;
     localStorage.setItem('brand', brand);
+    localStorage.setItem('car', car);
     bb.pushScreen('addModel.html', 'addModel', {'brand': brand}); 
 }
 
 function registerCarModel(model) {
     $("#activityindicator").slideToggle("slow", "swing");
     localStorage.setItem('model', model);
+    var car = new Object();
+    car = localStorage.getItem('car');
+    car.model = model;
+    console.log("registerCarModel() - car from localStorage:");
+    console.dir(car);
+    localStorage.setItem('car', car);    
     bb.pushScreen('addEngine.html', 'addEngine', {'model': model});
 }
 
 function registerEngineType(fueltype) {
     $("#activityindicator").slideToggle("slow", "swing");
     localStorage.setItem('fueltype', fueltype);
+    var car = new Object();
+    car = localStorage.getItem('car');
+    console.log("registerEngineType() - car from localStorage:");
+    console.dir(car);
+    car.fueltype = fueltype;
+    localStorage.setItem('car', car);
     bb.pushScreen('settings.html', 'settings');
+    
 }
 
 function registerSettings() {
+    console.log("registerSettings() - car from localStorage:");
     console.dir(localStorage.getItem('car'));
 
     if(localStorage.getItem('registrationFinished') == true) {
